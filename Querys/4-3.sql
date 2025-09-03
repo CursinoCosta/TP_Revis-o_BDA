2a6c432ac8d809fda3c36232ac9a72a72bbdd457
FelipeGomide <128003716+FelipeGomide@users.noreply.github.com>
2025-09-02 23:04:58 -0300

    Update 4-3.sql

--- Querys/4-3.sql
+++ Querys/4-3.sql
@@ -1,7 +1,8 @@
-SELECT CP.id_produto 
+SELECT CP.ID_produto 
 FROM Cliente AS C
-OIN Pedido AS P ON C.id = P.id_cliente 
-JOIN Carga AS CA ON P.id = CA.id_pedido 
-JOIN Carga_Produto AS CP ON CA.id_carga = CP.id_carga 
-WHERE C.Nome IN ('DCC', 'ICB') GROUP BY CP.id_produto 
+    JOIN Pedido AS P ON C.ID = P.ID_cliente 
+    JOIN Carga AS CA ON P.ID = CA.ID_pedido 
+    JOIN Carga_Produto AS CP ON CA.ID = CP.ID_Carga 
+WHERE C.Nome IN ('DCC', 'ICB')
+GROUP BY CP.id_produto 
 HAVING COUNT(DISTINCT C.Nome) = 2;

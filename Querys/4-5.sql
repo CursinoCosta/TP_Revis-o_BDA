e089d03e73127f05df4b5cd61ddaba9dbdb31e6e
FelipeGomide <128003716+FelipeGomide@users.noreply.github.com>
2025-09-02 23:10:18 -0300

    Update 4-5.sql

--- Querys/4-5.sql
+++ Querys/4-5.sql
@@ -1,14 +1,9 @@
-SELECT
-    P.*
-FROM
-    Pedido AS P
+SELECT *
+FROM Pedido AS P
 JOIN (
-    SELECT
-        ID_Pedido
-    FROM
-        Carga
-    GROUP BY
-        ID_Pedido
-    HAVING
-        COUNT(ID) > 1
-) AS CargasMultiplas ON P.Codigo = CargasMultiplas.ID_Pedido;
+    SELECT ID_Pedido
+    FROM Carga
+    GROUP BY ID_Pedido
+    HAVING COUNT(ID) > 1
+    ) AS CargasMultiplas 
+ON P.ID = CargasMultiplas.ID_Pedido;

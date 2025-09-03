2047dbc29286f9f81017d21ae1bafec846457ce6
FelipeGomide <128003716+FelipeGomide@users.noreply.github.com>
2025-09-02 22:46:34 -0300

    Update 3.sql

--- Querys/3.sql
+++ Querys/3.sql
@@ -2,9 +2,9 @@ WITH tab1 AS (
     SELECT *
     FROM Carga C
     LEFT JOIN Rota R ON C.ID_Rota = R.ID
-    LEFT JOIN Veiculo V ON R.ID_Veiculo = V.ID
+    LEFT JOIN Veiculo V ON R.ID_Veiculo = V.Chassi
 )
 
 SELECT A.Descricao
 FROM tab1 A
-LEFT JOIN tab1 B ON A.ID_Motorista =  B.ID_Motorista
+LEFT JOIN tab1 B ON A.CPF_Motorista =  B.CPF_Motorista
